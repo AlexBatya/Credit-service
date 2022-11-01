a5ed60eefefee8a59eb4aef0f5698085e4803f95
AlexBatya <alex28.12.12@mail.ru>
2022-11-02 01:13:55 +1000

    newCorrect

--- server.js
+++ server.js
@@ -9,22 +9,6 @@ var mainLink = require('./routers/CreditText.js');
 app.use(mainLink);
 app.get('/', function (req, res) {
     req.credit();
-    // res.render(file('main'),{
-    //     title: 'mainLink',
-    //     sizeCredit: 'Кредит от 1000 до 250 000 р',
-    //     num1: 18,
-    //     num2: 99 + '%',
-    //     num3: '2:00',
-    //     textCond1: 'Минимальный возраст заемщика', 
-    //     textCond2: 'Вероятность получения кредита',
-    //     textCond3: 'Минуты на получение денег',
-    //     promotionalRate: 0,
-    //     advertisementCredit: 'Успей оставить заявку и получи кредит под 0% годовых',
-    //     timeday: '00',
-    //     timehours: 23,
-    //     timeminuts: 54,
-    //     timeseconds: 22
-    // })
 });
 app.listen(3000, function () {
     console.log(color.green('Сервер запущен, батеньки...'));
